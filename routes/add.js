7209e95590dca966d31a8feec82fc0f139d59b07
Mayank <rajoria_m@yahoo.com>
2019-02-06 21:05:06 -0800

    done

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,9 @@
 var data = require("../data.json");
 
-exports.addFriend = function(req, res) {    
+exports.addFriend = function(request, response) {    
 	// Your code goes here
+	json = {'name': request.query.name,'description':request.query.description, 'imageURL':'http://lorempixel.com/400/400/people'};
+	console.log(json);
+	data.friends.push(json);
+	response.render('index', data);
  }
